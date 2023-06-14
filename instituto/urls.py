a6dad656b66c3a354babe269182307fffa525a57
Juan Figueroa <juan.figueroac@duocuc.cl>
2023-06-14 19:38:34 -0400

    -update: se actualizaron rutas y estructura

--- instituto/urls.py
+++ instituto/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from enexx import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('enexx.urls')), 
+    path('enexx/', include('enexx.urls')),
+    path('', views.index, name='index'),     
 
 ]
