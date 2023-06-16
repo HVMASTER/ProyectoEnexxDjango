e3c380ed50466a193b7f57970413531d1b545fb9
Manuus1 <106639853+Manuus1@users.noreply.github.com>
2023-06-16 15:54:28 -0400

    se agrega base de datos

--- enexx/models.py
+++ enexx/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Consola(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.nombre
+
+class Juego(models.Model):
+    nombre = models.CharField(max_length=50)
+    precio = models.IntegerField()
+    genero = models.TextField()
+    consola = models.ForeignKey(Consola, on_delete=models.PROTECT)
+
+    def __str__(self) -> str:
+        return self.nombre
