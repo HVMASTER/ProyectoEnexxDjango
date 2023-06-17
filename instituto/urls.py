1b54867951073cf52c1b58c05be6d7c293210672
Manuel <man.conchar@duocuc.cl>
2023-06-16 21:56:30 -0400

    Base de datos

--- instituto/urls.py
+++ instituto/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from enexx import views as enexx_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +29,5 @@ urlpatterns = [
     path('enexx/Switch_digital', enexx_views.Switch_digital, name='Switch_digital'),
     path('enexx/Switch_dlc', enexx_views.Switch_dlc, name='Switch_dlc'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
